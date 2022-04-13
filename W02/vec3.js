043a744b4fc6f459dc04b51faaedff1232b35d73
teradataiki <103481093+teradataiki@users.noreply.github.com>
2022-04-13 16:27:50 +0900

    Update vec3.js

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,11 +1,14 @@
-Vec3 = function( x, y, z )
+class Vec3
+{
+
+constructor(x,y,z)
 {
     this.x = x;
     this.y = y;
     this.z = z;
 }
 
-Vec3.prototype.add = function( v )
+add( v )
 {
     this.x += v.x;
     this.y += v.y;
@@ -13,7 +16,7 @@ Vec3.prototype.add = function( v )
     return this;
 }
 
-Vec3.prototype.sub = function( v )
+sub( v )
 {
     this.x -= v.x;
     this.y -= v.y;
@@ -21,30 +24,30 @@ Vec3.prototype.sub = function( v )
     return this;
 }
 
-Vec3.prototype.sum = function()
+sum()
 {
     return this.x + this.y + this.z;
 }
 
-Vec3.prototype.min = function()
+min()
 {
     //return Math.min( this.x, this.y, this.z );
     const m =  this.x < this.y ? this.x : this.y;
     return m < this.z ? m : this.z;
 }
 
-Vec3.prototype.max = function()
+max()
 {
     //return Math.max( this.x, this.y, this.z );
     const m = this.x > this.y ? this.x : this.y;
     return m > this.z ? this.z : m;}
 
-Vec3.prototype.mid = function()
+mid()
 {
     return this.sum() - this.min() - this.max();
 }
 
-Vec3.prototype.cross = function( v )
+cross( v )
 {
     var x = this.x, y = this.y, z = this.z;
     this.x = y * v.z - z * v.y;
@@ -53,7 +56,8 @@ Vec3.prototype.cross = function( v )
     return this;
 }
 
-Vec3.prototype.length = function()
+length()
 {
     return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+}
 }
